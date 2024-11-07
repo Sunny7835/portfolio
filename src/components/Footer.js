7a4c5a1d1cb0567d0bd8864b3df63abec916ad8b
sunny7835 <sunnysharmasvm2001@gmail.com>
2024-11-07 09:50:12 +0530

    index logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/portfolio.png";
 import navIconLinkedIn from "../assets/img/nav-icon1.svg";
 import navIconGit from "../assets/img/nav-icon-github.svg";
 // import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -12,12 +12,17 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            {/* <img src={logo} alt="Logo" /> */}
+            <img src={logo} alt="Logo" />
           </Col>
+
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIconLinkedIn} alt="Icon" /></a>
-              <a href="#"><img src={navIconGit} alt="Icon" /></a>
+              <a href="#">
+                <img src={navIconLinkedIn} alt="Icon" />
+              </a>
+              <a href="#">
+                <img src={navIconGit} alt="Icon" />
+              </a>
               {/* <a href="#"><img src={navIcon3} alt="Icon" /></a> */}
             </div>
             <p>Copyright 2024. All Rights Reserved</p>
@@ -25,5 +30,5 @@ export const Footer = () => {
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/portfolio.png";
 import navIconLinkedIn from "../assets/img/nav-icon1.svg";
 import navIconFB from "../assets/img/nav-icon2.svg";
 import navIconInsta from "../assets/img/nav-icon3.svg";
@@ -36,7 +36,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            {/* <img src={logo} alt="Logo" /> */}
+            <img src={logo} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
